3550a01f1352a87d8961d4ab957175b54448e241
Dan Sehl <dan.sehl@gmail.com>
2020-06-30 18:34:40 -0600

    Refactor index method in shelters_controller file, add #show method

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,10 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
+    @shelters = Shelter.all
   end
+
+  def show
+    @shelter = Task.find(params[:id])
+  end
+
 end
