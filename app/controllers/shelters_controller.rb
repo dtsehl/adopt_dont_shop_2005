0bac7a0cd35a9d2be73bef233f886d1031e85ee3
Dan Sehl <dan.sehl@gmail.com>
2020-07-01 19:07:58 -0600

    Refactor #create method in shelters_controller, add #shelter_params private method

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,19 +7,18 @@ class SheltersController < ApplicationController
   end
 
   def create
-    shelter = Shelter.new({
-      name: params[:shelter][:name],
-      address: params[:shelter][:address],
-      city: params[:shelter][:city],
-      state: params[:shelter][:state],
-      zip: params[:shelter][:zip]
-      })
-    shelter.save
+    Shelter.create(shelter_params)
     redirect_to '/shelters'
   end
 
   def show
     @shelter = Shelter.find(params[:id])
+  end
+
+  private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
