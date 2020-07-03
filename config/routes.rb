a68738729ba05b0393b7153449602506e0b878f8
Dan Sehl <dan.sehl@gmail.com>
2020-07-03 16:43:42 -0600

    Add route for shelter_pets

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/pets', to: 'pets#index'
-  
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-
+  get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
 end
