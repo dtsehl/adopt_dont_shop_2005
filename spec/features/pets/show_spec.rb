c4679bca85ca789465384de3f42b8bfae75b7fc1
Dan Sehl <dan.sehl@gmail.com>
2020-07-03 17:17:22 -0600

    Refactor show_spec for pets to have accurate visit command

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "pets detail page", type: :feature do
     shelter_1 = Shelter.create(name: "Joe's Shelter", address: "123 Apple St.", city: "Denver", state: "CO", zip: 80202, id: 1)
     pet_1 = shelter_1.pets.create(image: "/Users/dan/turing/2module/adopt_dont_shop_2005/app/assets/images/afghanhound_dog_pictures_.jpg", name: "Fido", approx_age: 3, sex: "F", shelter_name: shelter_1.name, description: "A furry friend!", status: true)
 
-    visit "/shelters/#{pet_1.id}"
+    visit "/pets/#{pet_1.id}"
 
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.description)
