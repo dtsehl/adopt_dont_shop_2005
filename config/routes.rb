65a7045cc87d6df5672a03b24b6ff72f2f017508
Dan Sehl <dan.sehl@gmail.com>
2020-07-03 20:50:42 -0600

    Add pets#edit and pets#update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
+  get '/pets/:id/edit', to: 'pets#edit'
+  patch 'pets/:id', to: 'pets#update'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
